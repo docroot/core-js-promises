b996c48146033d7cc3b295bddbdf586d1e6c890e
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2025-04-14 13:38:18 +0400

    feat: add solutions for last two tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -113,8 +113,10 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) =>
+    results.map((r) => (r.status === 'fulfilled' ? r.value : null))
+  );
 }
 
 /**
@@ -135,8 +137,12 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce(async (acc, p) => {
+    const resultString = await acc;
+    const val = await p;
+    return resultString + val;
+  }, Promise.resolve(''));
 }
 
 module.exports = {
